ead2d2d00a54d3810a53d88a4e295f3467185138
Max Greene <mjgreene27@gmail.com>
2020-04-03 11:37:41 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -2,4 +2,16 @@ const cards = ["queen", "queen", "king", "king"];
 let cardOne = cards[0];
 const cardsInPlay = [];
 cardsInPlay.push('cardOne');
-console.log("User flipped " + cards[0]);
+console.log("User flipped queen");
+let cardTwo = cards[3];
+cardsInPlay.push('cardTwo');
+console.log("user flipped king");
+
+if (cardsInPlay.length === 2) {
+ if (cardsInPlay[0] === cardsInPlay[1]) {
+	alert("You found a match!");
+} else if (cardsInPlay[0] !== cardsInPlay[1]) {
+	alert("Sorry, try again.");
+}
+}
+
