05f5c7a82e28fd8e528daddc6a526e8fa95eafa2
Max Greene <mjgreene27@gmail.com>
2020-04-03 15:28:18 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,17 +1,21 @@
-const cards = ["queen", "queen", "king", "king"];
-let cardOne = cards[0];
-const cardsInPlay = [];
-cardsInPlay.push('cardOne');
-console.log("User flipped queen");
-let cardTwo = cards[3];
-cardsInPlay.push('cardTwo');
-console.log("user flipped king");
+const cards = ["queen", "queen", "king", "king"]; // first array
+const cardsInPlay = []; // second array
+cardsInPlay.push(cards[0]);
+cardsInPlay.push(cards[2]);
 
+function flipCard (cardId) { 
+	console.log("User flipped " + cards[0]);
+	console.log("User flipped " + cards[2]);
 if (cardsInPlay.length === 2) {
- if (cardsInPlay[0] === cardsInPlay[1]) {
-	alert("You found a match!");
-} else if (cardsInPlay[0] !== cardsInPlay[1]) {
-	alert("Sorry, try again.");
-}
-}
+checkForMatch();
+}};
 
+flipCard(0);
+flipCard(1);
+
+function checkForMatch (){
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+  console.log("You found a match!");
+} else {
+  console.log("Sorry, try again.");
+}};
