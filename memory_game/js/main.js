9b4c266ac0161ac175e80bc31fc317f2f987303f
Max Greene <mjgreene27@gmail.com>
2020-04-05 15:28:28 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -24,19 +24,18 @@ const cardsInPlay = []; // second array
 cardsInPlay.push(cards[0].rank);
 cardsInPlay.push(cards[2].rank);
 
-function flipCard (cardId) { 
+function flipCard () { 
+	let cardId = this.getAttribute('data-id');
 	console.log("User flipped " + cards[0].rank);
 	console.log("User flipped " + cards[2].rank);
 	console.log(cards[0].cardImage);
 	console.log(cards[0].suit);
 	console.log(cards[2].cardImage);
 	console.log(cards[2].suit);
+	this.getAttribute('cards').setAttribute('src', 'cards[cardId].cardImage');
 if (cardsInPlay.length === 2) {
 checkForMatch();
 }};
-
-flipCard(0);
-flipCard(1);
 
 function checkForMatch (){
 	if (cardsInPlay[0] === cardsInPlay[1]) {
@@ -44,3 +43,14 @@ function checkForMatch (){
 } else {
   alert("Sorry, try again.");
 }};
+
+function createBoard (){
+for (let i = 0; i < cards.length; i++) {
+   let cardElement = document.createElement('img');
+   cardElement.setAttribute('src', "images/back.png");
+   cardElement.setAttribute('data-id', i);
+   cardElement.addEventListener('click', flipCard);
+   document.getElementById('game-board').appendChild(cardElement);
+}
+};
+createBoard(0, 2);
